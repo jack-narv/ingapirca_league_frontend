0a4ceb2f6c06267c5428bc82fe9e3371463f1b6a
Jack <jackricardonarvaezsalazar@gmail.com>
2026-02-24 23:23:17 -0500

    Change name of app

--- linux/runner/my_application.cc
+++ linux/runner/my_application.cc
@@ -45,11 +45,11 @@ static void my_application_activate(GApplication* application) {
   if (use_header_bar) {
     GtkHeaderBar* header_bar = GTK_HEADER_BAR(gtk_header_bar_new());
     gtk_widget_show(GTK_WIDGET(header_bar));
-    gtk_header_bar_set_title(header_bar, "ingapirca_league_frontend");
+    gtk_header_bar_set_title(header_bar, "NextLiga");
     gtk_header_bar_set_show_close_button(header_bar, TRUE);
     gtk_window_set_titlebar(window, GTK_WIDGET(header_bar));
   } else {
-    gtk_window_set_title(window, "ingapirca_league_frontend");
+    gtk_window_set_title(window, "NextLiga");
   }
 
   gtk_window_set_default_size(window, 1280, 720);
