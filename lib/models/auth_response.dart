54638fda901e821fcf7a4b55de441b7411d67dca
Jack <jackricardonarvaezsalazar@gmail.com>
2026-03-01 21:36:44 -0500

    Keep session saved

--- lib/models/auth_response.dart
+++ lib/models/auth_response.dart
@@ -1,15 +1,18 @@
 class AuthResponse {
   final String accessToken;
+  final String? refreshToken;
   final List<String> roles;
 
   AuthResponse({
     required this.accessToken,
+    this.refreshToken,
     required this.roles,
   });
 
   factory AuthResponse.fromJson(Map<String, dynamic> json) {
     return AuthResponse(
-      accessToken: json['accessToken'],
+      accessToken: (json['accessToken'] ?? '').toString(),
+      refreshToken: json['refreshToken']?.toString(),
       roles: List<String>.from(json['user']['roles']),
     );
   }
