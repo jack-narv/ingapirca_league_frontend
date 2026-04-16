3ee74945acde44ae22b24f87bbfa68283c2e9ed3
Jack <jackricardonarvaezsalazar@gmail.com>
2026-04-15 23:23:30 -0500

    Standings, statistics and sanctions features

--- lib/models/suspension_summary.dart
+++ lib/models/suspension_summary.dart
@@ -2,6 +2,8 @@ class SuspensionSummary {
   final String playerId;
   final String firstName;
   final String lastName;
+  final String? teamId;
+  final String? teamName;
   final int? shirtNumber;
   final int pendingMatchesSuspended;
 
@@ -9,6 +11,8 @@ class SuspensionSummary {
     required this.playerId,
     required this.firstName,
     required this.lastName,
+    required this.teamId,
+    required this.teamName,
     required this.shirtNumber,
     required this.pendingMatchesSuspended,
   });
@@ -20,6 +24,14 @@ class SuspensionSummary {
       playerId: (json['player_id'] ?? '').toString(),
       firstName: (json['first_name'] ?? '').toString(),
       lastName: (json['last_name'] ?? '').toString(),
+      teamId: (json['team_id'] ?? json['teamId'])?.toString(),
+      teamName:
+          (json['team_name'] ??
+                  json['teamName'] ??
+                  (json['teams'] is Map<String, dynamic>
+                      ? (json['teams'] as Map<String, dynamic>)['name']
+                      : null))
+              ?.toString(),
       shirtNumber: json['shirt_number'] == null
           ? null
           : int.tryParse(json['shirt_number'].toString()),
